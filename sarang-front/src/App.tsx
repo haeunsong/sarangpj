a2ee185ba10fefd1843fc3c6a8fe9576c0fc108b
haeunsong <jamy022576@gmail.com>
2024-01-04 15:40:20 +0900

    컴포넌트 생성(게시물 리스트 아이템) + css 설정하기

---
+++ sarang-front/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BoardListItem from "components/BoardListItem";
+
+function App() {
+  return (
+    <>
+      <BoardListItem />
+    </>
+  );
+}
+
+export default App;
